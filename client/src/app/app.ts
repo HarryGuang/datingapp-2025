91390d40640599a6124dcb274140588434df05cd
Harry Guang <--get>
2025-11-24 14:25:44 -0500

    End of section 6

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,39 +1,16 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { Nav } from "../layout/nav/nav"
-import { AccountService } from '../core/service/account-service';
-import { Home } from '../features/home/home';
-import { User } from '../types/user';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
-  protected readonly title = 'Dating app';
-  protected members = signal<User[]>([]);
+export class App {
+  protected router = inject(Router);
   
-  // Observable approach
-  ngOnInit(): void {
-    this.http.get<User[]>('https://localhost:5001/api/members').subscribe({
-      next: response => this.members.set(response),
-      error: err => console.error(err),
-      complete: () => console.log('Request completed')
-    });
-    this.setCurrentUser();
-  }
-
-  setCurrentUser() {
-    const userString = localStorage.getItem("user");
-    if (!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-
   /*
   // Promise approach
   async ngOnInit(): Promise<void> {
