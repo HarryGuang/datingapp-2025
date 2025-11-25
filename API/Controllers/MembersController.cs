864e05234d5530241eaa6208f468fdf102a47ec2
Harry Guang <--get>
2025-11-25 01:58:44 -0500

    End of section 8

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -3,26 +3,32 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
+using API.Interfaces;
 
 namespace API.Controllers
 {
-    public class MembersController(AppDbContext context) : BaseApiController
+    [Authorize]
+    public class MembersController(IMemberRepository memberRepository) : BaseApiController
     {
         // GET: api/members
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers()
         {
-            var members = await context.Users.ToListAsync();
-            return members;
-        }
+            return Ok(await memberRepository.GetMembersAsync());
+        }        
         
-        [Authorize]
         [HttpGet("{id}")] // locahost:5000/api/members/bob-id
-        public async Task<ActionResult<AppUser>> GetMember(string id)
+        public async Task<ActionResult<Member>> GetMember(string id)
         {
-            var member = await context.Users.FindAsync(id);
+            var member = await memberRepository.GetMemberByIdAsync(id);
             if (member == null) return NotFound();
             return member;
+        }
+
+        [HttpGet("{id}/photos")] // locahost:5000/api/members/bob-id
+        public async Task<ActionResult<IReadOnlyList<Photo>>> GetMemberPhotos(string id)
+        {
+            return Ok(await memberRepository.GetPhotosForMemberAsync(id));
         }
     }
 }
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,14 +1,17 @@
 using System;
 using System;
 
-namespace API.Entities
+namespace API.Entities;
+
+public class AppUser
 {
-    public class AppUser
-    {
-        public string Id { get; set; } = Guid.NewGuid().ToString();
-        public required string DisplayName { get; set; }
-        public required string Email { get; set; }
-        public required byte[] PasswordHash { get; set; }
-        public required byte[] PasswordSalt { get; set; }
-    }
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public required string DisplayName { get; set; }
+    public required string Email { get; set; }
+    public string? ImageUrl { get; set; }
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
+
+    // Navigation Property
+    public Member Member { get; set; } = null!;
 }
